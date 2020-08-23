f2dc81f16b4c93b1f1f7dcd2e7fad2def90a5acf
Leigh Halliday <leighhalliday@gmail.com>
2020-08-23 19:11:06 -0400

    Removing console.log

--- src/appsignal.ts
+++ src/appsignal.ts
@@ -1,7 +1,5 @@
 import Appsignal from "@appsignal/javascript";
 
-console.log(process.env.NEXT_PUBLIC_APPSIGNAL_FRONTEND_KEY);
-
 export const appsignal = new Appsignal({
   key: process.env.NEXT_PUBLIC_APPSIGNAL_FRONTEND_KEY,
   revision: process.env.BUILD_ID,
